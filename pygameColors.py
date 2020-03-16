a16f978ca317521f87e68ca9575c28a61c52a7b6
Rafael Vieira Coelho <rafaelvc2@gmail.com>
2020-03-16 10:30:44 -0300

    Add files via upload

---
+++ pygameColors.py
@@ -0,0 +1,4 @@
+from pygame.color import THECOLORS  # 600 color names
+
+for key in sorted(THECOLORS):
+	print(key, THECOLORS[key])  # sorted print
---
+++ pygameSimple.py
@@ -0,0 +1,35 @@
+#importa os módulos necessários
+import pygame
+from pygame.locals import *
+
+#inicia o pygame
+pygame.init()
+
+#define o tamanho e cor da janela
+screenSize = (640, 480)
+screen = pygame.display.set_mode(screenSize)
+screen.fill((255, 255, 255))
+
+# define o título
+pygame.display.set_caption("Hello, World!")
+
+#cria o relógio que controla o tempo do jogo
+clock = pygame.time.Clock()
+
+running = True
+while running:
+	#define velocidade
+	time_passed = clock.tick(30)
+	
+	#trata eventos
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			running = False
+	
+	#atualiza o estado do jogo
+	
+	#redesenha o jogo
+	pygame.display.update()
+
+#finaliza o pygame
+pygame.quit()		
